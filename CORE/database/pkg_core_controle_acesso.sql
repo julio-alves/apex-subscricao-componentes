8aef7853561709f5b81aca7f59a06228e38024d7
Julio Nonato <juliocnalves1@gmail.com>
2024-11-25 19:22:04 -0300

    Add tabela para log após autenticação

--- CORE/database/pkg_core_controle_acesso.sql
+++ CORE/database/pkg_core_controle_acesso.sql
@@ -1,12 +1,15 @@
-CREATE OR REPLACE PACKAGE PKG_CORE_CONTROLE_ACESSO IS
+create or replace PACKAGE PKG_CORE_CONTROLE_ACESSO IS
   FUNCTION set_encrypted_password (p_username    IN VARCHAR2,
                                    p_password    IN VARCHAR2) RETURN RAW;
 
   FUNCTION authentication ( p_username IN VARCHAR2
                           , p_password IN VARCHAR2)  RETURN BOOLEAN;
+  
+  PROCEDURE registrar_log_autenticacao ( p_usuario       IN VARCHAR2
+                                       , p_cod_aplicacao IN VARCHAR2);
 END;
 /
-CREATE OR REPLACE PACKAGE BODY PKG_CORE_CONTROLE_ACESSO IS
+create or replace PACKAGE body PKG_CORE_CONTROLE_ACESSO IS
 
     FUNCTION set_encrypted_password (p_username    IN VARCHAR2,
                                      p_password    IN VARCHAR2) RETURN RAW IS
@@ -39,6 +42,18 @@ CREATE OR REPLACE PACKAGE BODY PKG_CORE_CONTROLE_ACESSO IS
   EXCEPTION
     WHEN no_data_found THEN
       RETURN FALSE; -- Usuário não encontrado..
+  END;
+
+  PROCEDURE registrar_log_autenticacao ( p_usuario       IN VARCHAR2
+                                       , p_cod_aplicacao IN VARCHAR2) IS
+  BEGIN
+    INSERT INTO CORE_log_autenticacao (usuario_autenticacao, data_autenticacao, cod_aplicacao)
+    VALUES (p_usuario, SYSDATE, p_cod_aplicacao);
+    
+    COMMIT; 
+  EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Erro ao registrar o log de autenticação: ' || SQLERRM);
   END;
 
 END;
